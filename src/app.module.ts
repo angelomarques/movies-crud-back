cc9077cf40a0257e3214dd902f0ea54fb8ff2f0f
Ângelo Marques <angeloemanuelmarques@gmail.com>
2025-05-29 23:47:11 -0300

    set up validation pipe and typeorm with postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.TYPEORM_URL,
+      entities: [`${__dirname}/**/entities/*.{ts,js}`],
+      migrations: [`${__dirname}/**/migration/*.{ts,js}`],
+      logging: false,
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
